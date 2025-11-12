6965310d66c1cf2575c732ca186c487c889e5232
Melinda B <melindabankovic@gmail.com>
2025-11-12 16:15:55 +0100

    end of first day

---
+++ Tamagotchi/Assets/#project/scripts/Alerts.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using TMPro;
+
+
+
+public class Alerts : MonoBehaviour
+{
+
+    [SerializeField] private GameManager gameManager;
+    public TextMeshProUGUI MamieText;
+    public TextMeshProUGUI AssuranceText;
+    public TextMeshProUGUI NoelText;
+
+
+    //recup salary
+  
+
+    // donne des alertes toutes les x tps pour donner nouvelles aléatoires 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void AlertMamie()
+    {
+        MamieText.gameObject.SetActive(true);
+
+        MamieText.text = "Ta mamie t'a envoyé 150 euros !";
+
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            MamieText.gameObject.SetActive(false);
+
+        }
+
+        gameManager.add(150);
+        
+    }
+
+    void AlertAssurance()
+    {
+        AssuranceText.gameObject.SetActive(true);
+
+        AssuranceText.text = "Ta compagnie d'assurance te réclame 30 euros";
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            AssuranceText.gameObject.SetActive(false);
+
+        }
+
+        gameManager.retire(300);
+    }
+
+    void AlertNoel()
+    {
+        NoelText.gameObject.SetActive(true);
+
+        NoelText.text = "C'est Noel! Tu débourse 100 euros pour les cadeaux";
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            NoelText.gameObject.SetActive(false);
+
+        }
+
+        gameManager.retire(100);
+    }
+}
