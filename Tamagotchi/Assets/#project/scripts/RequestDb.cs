0f5ea1cf20a259fb56cd7e8c8844e67c9ce0b4bf
Msource <m.vandermeirsch@interface3.be>
2025-11-13 11:38:20 +0100

    request database functionnal

--- Tamagotchi/Assets/#project/scripts/RequestDb.cs
+++ Tamagotchi/Assets/#project/scripts/RequestDb.cs
@@ -2,27 +2,34 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
 using System;
+using System.Collections.Generic;
 
 //{"type":"general","setup":"Why are pirates called pirates?","punchline":"Because they arrr!","id":304}
 [Serializable]
-public struct Joke
+public struct Depenses
 {
-    //public int id;
-   // public string type;
-    public string setup;
-    public string punchline;
+    public Depense[] member;
+    public int totalItems;
+}
+
+[Serializable]
+public struct Depense
+{
+
+    public string typeDepense;
+    public string id;
 
 }
 
 public class RequestDb : MonoBehaviour
 {
-    private string url = "https://official-joke-api.appspot.com";
+    private string url = "https://pitifully-ticketless-fabiola.ngrok-free.dev/api";
     private string jsonData;
 
     void Start()
     {
 
-        StartCoroutine(GetRequest(url + "/random_joke"));
+        StartCoroutine(GetRequest(url + "/depenses"));
     }
 
     IEnumerator GetRequest(string url)
@@ -46,9 +53,9 @@ public class RequestDb : MonoBehaviour
     private void ReadData(UnityWebRequest uwr)
     {
         jsonData = uwr.downloadHandler.text;
-        Joke reallyGoodJoke = JsonUtility.FromJson<Joke>(jsonData);
-        Debug.Log(reallyGoodJoke.setup);  //--> ligne à décortiquer
-        Debug.Log(reallyGoodJoke.punchline);
+        Depenses data = JsonUtility.FromJson<Depenses>(jsonData);
+        Debug.Log(data.totalItems);  //--> ligne à décortiquer
+        Debug.Log(data.member[0].@id);
     }
 
  
